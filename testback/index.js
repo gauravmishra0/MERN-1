de4855e485ef4e3eaf16514ab931c0d381903e67
Gaurav mishra <garvmishra9@gmail.com>
2021-07-11 21:21:23 +0530

    started

---
+++ testback/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+const port = 2001;
+
+app.get("/", (req, res) => {
+    return res.send("Hey man!")
+});
+app.get("/login", (req, res) => {
+    return res.send("You are logged in!")
+});
+app.get("/sign-out", (req, res) => {
+    return res.send("You are signed out!")
+});
+app.get("/hitesh", (req, res) => {
+    return res.send("IG fan")
+});
+
+app.listen(port, () =>{
+    console.log("Boom your first server");
+})
